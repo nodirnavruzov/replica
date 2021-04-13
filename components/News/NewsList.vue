128ef260936a755e1c72f63960ed69c5943341e5
Nodir Navruzov <spy.nowin@gmail.com>
2021-04-13 16:35:33 +0500

    finish news

---
+++ components/News/NewsList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="news__container col-md-8">
+        <div class="news__filter-wrap"></div>
+        <NewsPreview v-for="nw in news" :key="nw.id" :news="nw"> </NewsPreview>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NewsPreview from "~/components/News/NewsPreview";
+
+export default {
+  components: { NewsPreview },
+
+  props: {
+    news: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
